1a6b9c81737c72fe01d21a220c261a90c5499d1a
Buğra Öner <boletroy@gmail.com>
2021-11-05 02:09:15 +0300

    Diziler

---
+++ Dizi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dizi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}d
