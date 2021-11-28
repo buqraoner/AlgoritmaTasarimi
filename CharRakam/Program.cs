9e96b55fb98ff209c7dc79d101efa892358438f2
Buğra Öner <boletroy@gmail.com>
2021-11-28 23:30:32 +0300

    CharRakam

---
+++ CharRakam/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CharRakam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 65; i<=90; i++){
+                Console.Write("{0,5}",
+                (char)i);
+
+                if(i % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
