75556b27f9e5625a8d7329dc2bdd5cbb1abc01f8
Buğra Öner <boletroy@gmail.com>
2021-12-03 15:10:52 +0300

    String

--- String/Program.cs
+++ String/Program.cs
@@ -8,17 +8,31 @@ namespace String
         {
             Console.WriteLine("String İfadeler");
             Console.WriteLine("Kelime Bulucu ");
-            string s1 = "This  string consist of a single short sentence.";
+            string s1 = "This string consist of a single short sentence.";
             int nWords = 0;
 
             s1 = s1.Trim();
+            // Başında veya solunda boşluk varsa trimle
+
             for(int ctr=0; ctr <s1.Length; ctr++){
                 if(Char.IsPunctuation(s1[ctr]) | Char.IsWhiteSpace(s1[ctr]))
                 nWords++;
             }
 
+            // 0'dan başla s1'in uzunluğu kadar dön ve karakter sayısını arttır
+            //for döngüsü T'ye H'ye bakarak tek tek ilerleyecek.
+            // char.IsPunctuation , 
+            // Unicode karakterinin bir noktalama işareti olarak kategorilere ayrılmadığını gösterir.
+
+            //||(veya) operatörü iki değerden birisi doğru ise doğru, ikisi de yanlış ise yanlış değerini döndürür ayrıca önceliği en az olan mantıksal operatördür.
+
+            //Char.IsWhiteSpace Boşluk var mı yok mu 
             Console.WriteLine("The sentence \n  {0}\nhas {1} words.",
             s1,nWords);
+
+            //IsNullOrWhiteSpace bir dizenin  null,eşit String.Empty
+            //veya yalnızca boşluk karakterlerinden oluşan bir değer olup olmadığını gösterir
+            
             
 
 
