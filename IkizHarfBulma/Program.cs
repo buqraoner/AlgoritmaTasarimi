c9902a25fc0ad75a93ec87691c8a0d628932114e
Buğra Öner <boletroy@gmail.com>
2021-12-05 07:08:08 +0300

    İkiz harf bulma

---
+++ IkizHarfBulma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IkizHarfBulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
