28bca0269fde7b6aa212aad0a617335de7bd255b
Buğra Öner <boletroy@gmail.com>
2021-12-05 16:55:50 +0300

    IkizHarfBulma

--- IkizHarfBulma/Program.cs
+++ IkizHarfBulma/Program.cs
@@ -6,7 +6,37 @@ namespace IkizHarfBulma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //string veri tipine değer atama
+            string kelime = " Helo Worldd ";
+            
+            //Başı ve sonundaki boşlukları sil 
+            kelime = kelime.Trim();
+
+            //For döngüsü ile her harfi döndürme
+            //i tam sayı değerini  kelime stringin  harf uzunluğu kadar döndür
+            // her seferinde i'yi 1 arttır karakter bittiğinde döngüden çık
+            for(int i = 0; i<kelime.Length; i++)
+            {   
+
+                if(kelime[i]    == kelime[i+1]){
+
+                    Console.WriteLine(kelime[i]);
+                    break;
+
+                }
+
+               
+
+
+                }
+
+            }
+
+
+
+
+
+
         }
     }
-}
+
