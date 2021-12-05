43fce8ae2e4112b7547bd4e5e49960d5c59e7c82
Buğra Öner <boletroy@gmail.com>
2021-12-05 18:27:43 +0300

    FirstRepeated

---
+++ IkizHarfBulma2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace IkizHarfBulma2
+{
+    class Program
+    {
+
+         static int TekrarEden(string s)
+        {
+
+            int a = -1;
+            int i = 0;
+
+            for (int j = 0;  j<s.Length; j++)
+            {
+
+                for (j = i + 1; j < s.Length; j++)
+                {
+
+               
+                    if (s[i] == s[j])
+                {
+                    a = j;
+                    break;
+                }
+
+                }
+                if (a != -1)          
+                break;
+
+            }
+            return a;
+
+
+            }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir string ifade giriniz");
+        string kelime = Console.ReadLine();
+            int deneme = TekrarEden(kelime);
+            if (deneme == -1)
+                Console.WriteLine("Not found");
+            else
+                Console.WriteLine(kelime[deneme]);
+            
+            
+            // Fonkisyonu çağır
+
+
+        }
+    }
+}
